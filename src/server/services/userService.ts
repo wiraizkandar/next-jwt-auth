772085e14f5ca805a901319424c6bcee40c5b731
wira izkandar <wiraizkandar@wiras-MacBook-Pro.local>
2023-12-11 17:50:40 +0800

    update

--- src/server/services/userService.ts
+++ src/server/services/userService.ts
@@ -1,33 +1,33 @@
 export const userService = {
-    authenticate,
-  };
-  
-  async function authenticate(username: string, password: string) {
+	authenticate,
+};
 
-    console.log("BABI");
+async function authenticate(username: string, password: string) {
+	try {
+		// Create a new URLSearchParams object
+		const formData = new URLSearchParams();
 
-    try {
-        // Create a new URLSearchParams object
-const formData = new URLSearchParams();
+		// Add form fields and values to the URLSearchParams object
+		formData.append("username", "ultron");
+		formData.append("password", "password");
 
-        // Add form fields and values to the URLSearchParams object
-        formData.append('username', 'ultron');
-        formData.append('password', 'password');
+		process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-        const authenticateRequest = await fetch('https://hostedapi.test/api/v1/auth/authenticate',{
-            method : "POST",
-            headers : {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: formData.toString(),
-        });
+		const authenticateRequest = await fetch(
+			"https://hostedapi.test/api/v1/auth/authenticate",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: formData.toString(),
+			}
+		);
 
-        const user = await authenticateRequest.json();
+		const user = await authenticateRequest.json();
 
-        return user;
-        
-    } catch (error) {
-        console.log(error);
-    }
-
-  }
+		return user;
+	} catch (error) {
+		console.log(error);
+	}
+}
